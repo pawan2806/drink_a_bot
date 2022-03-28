16409663e97069b87cfc727e1490bf7a8ecf0abc
Pawan <pawan.singh.co19@nsut.ac.in>
2022-03-28 21:52:35 +0530

    fking changes

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -3,7 +3,6 @@ import 'package:drink_a_bot/helpers/constants.dart';
 import 'package:flutter/material.dart';
 //import 'package:assets_audio_player/assets_audio_player.dart';
 
-
 class LandingPage extends StatefulWidget {
   static String id = 'home_screen';
   @override
@@ -11,110 +10,126 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
-
   //var firstColor = Color(0xffdecdc3), secondColor = Color(0xffea5455);
   //var firstColor = Color(0xFF07689f),secondColor = Color(0xffa2d5f2);
-  var firstColor = Color(0xFFeb8f8f),secondColor = Color(0xFFec0101);
-  var code ;
-
-
-
-
+  var firstColor = Color(0xFFeb8f8f), secondColor = Color(0xFFec0101);
+  var code;
 
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image:AssetImage('images/background.jpg'),
-                fit: BoxFit.cover
-            )
+            color:  Colors.black,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Flexible(
-              child:Column(
+            Expanded(
+              child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(
                       height: 40.0,
                     ),
                     Padding(
-                      padding: EdgeInsets.only(top: 50.0,left:10.0),
+                      padding: EdgeInsets.only(top: 50.0, left: 10.0),
                       child: Container(
-
-                        child: Text(
-                            'Crazy LED',
+                        alignment: Alignment.center,
+                        child: Text('Drink-a-Bot',
+                            textAlign: TextAlign.center,
                             style: TextStyle(
                               //fontFamily: 'Montserrat',
-                              color: Colors.white,
+                              color: Colors.white70,
+                              fontWeight: FontWeight.bold,
                               fontSize: 50.0,
-                            )
-                        ),
+                            )),
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(top: 10.0,left:10.0),
-                      child: Text(
-                          'Lighting for every mood.',
-                          style: TextStyle(
-                            //fontFamily: 'Montserrat',
-                            color: Colors.white,
-                            fontSize:20.0,
-                          )
+                      padding: EdgeInsets.only(top: 10.0, left: 10.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text('Smart Movable Dispenser',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              //fontFamily: 'Montserrat',
+                              color: Colors.white70,
+                              fontWeight: FontWeight.bold,
+            
+                              fontSize: 25.0,
+                            )),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 10.0, left: 10.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                                height: 300.0,
+                                //width: 200.0,
+                                decoration: new BoxDecoration(
+                                  image: new DecorationImage(
+                                    image: AssetImage('assets/img1.jpeg'),
+                                    //ExactAssetImage('assets/img1.jpeg'),
+                                    //fit: BoxFit.fitHeight,
+                                  ),
+                                )),
+                          ],
+                        ),
                       ),
                     )
-                  ]
-              ),
+                  ]),
             ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(bottom:30.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                      child: Card(
-                        margin: EdgeInsets.symmetric(vertical: 0.0,horizontal: 25.0),
-                        color: redShade,
-                        child: FlatButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => MainPage()),
-                            );
-                          },
-                          child: ListTile(
-                            title: Center(
-                              child: Text(
-                                "Connect Device",
-                                style: TextStyle(
-                                  //fontFamily: 'Montserrat',
-                                  fontSize: 17.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Card(
+                      margin: EdgeInsets.symmetric(
+                          vertical: 0.0, horizontal: 25.0),
+                      color: redShade,
+                      child: FlatButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MainPage()),
+                          );
+                        },
+                        child: ListTile(
+                          title: Center(
+                            child: Text(
+                              "Connect Device",
+                              style: TextStyle(
+                                //fontFamily: 'Montserrat',
+                                fontSize: 17.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
                         ),
                       ),
                     ),
-
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                ],
               ),
             )
           ],
-
         ),
       ),
     );
